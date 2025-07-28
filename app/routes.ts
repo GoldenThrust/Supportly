8faa7c5a196046297e16e41fa79dd77aca37c414
Adeniji Olajide <adenijiolajid01@gmail.com>
2025-07-29 00:23:20 +0100

    feat: Comment out isAuthenticated layout in routes for future use

--- app/routes.ts
+++ app/routes.ts
@@ -7,15 +7,15 @@ import {
 
 export default [
   layout("./auth/authprovider.tsx", [
-    layout("./auth/isAuthenticated.tsx", [
-      index("routes/home.tsx"),
-      route("book-session", "routes/book-session.tsx"),
-      route("booking-confirmation", "routes/booking-confirmation.tsx"),
-      route("dashboard", "routes/dashboard.tsx"),
-      route("profile", "routes/profile.tsx"),
-      route("video-call/:sessionId", "routes/video-call.$sessionId.tsx"),
-      route("logout", "routes/auth/logout.tsx"),
-    ]),
+    // layout("./auth/isAuthenticated.tsx", [
+    index("routes/home.tsx"),
+    route("book-session", "routes/book-session.tsx"),
+    route("booking-confirmation", "routes/booking-confirmation.tsx"),
+    route("dashboard", "routes/dashboard.tsx"),
+    route("profile", "routes/profile.tsx"),
+    route("video-call/:sessionId", "routes/video-call.$sessionId.tsx"),
+    route("logout", "routes/auth/logout.tsx"),
+    // ]),
     layout("routes/auth/layout.tsx", [
       route("login", "routes/auth/login.tsx"),
       route("signup", "routes/auth/signup.tsx"),
